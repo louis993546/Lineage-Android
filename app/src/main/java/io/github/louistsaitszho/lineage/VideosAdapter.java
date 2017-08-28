0d7f63c45c184cabca18d2ce655a977f92a37c36
Louis <louis993546@gmail.com>
2017-08-28 08:05:23 +0200

    ADD: Videos Adapter in Java

---
+++ app/src/main/java/io/github/louistsaitszho/lineage/VideosAdapter.java
@@ -0,0 +1,32 @@
+package io.github.louistsaitszho.lineage;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by louistsai on 27.08.17.
+ */
+public class VideosAdapter extends RecyclerView.Adapter {
+    public VideosAdapter() {
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
