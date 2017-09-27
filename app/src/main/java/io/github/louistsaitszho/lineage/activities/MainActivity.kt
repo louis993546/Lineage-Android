04f22c745f3fe28df95808946553e89290012554
lsteamer <lsteamer@gmail.com>
2017-09-27 14:24:59 +0200

    09/27 - Addressed most of the comments made.

--- app/src/main/java/io/github/louistsaitszho/lineage/activities/MainActivity.kt
+++ app/src/main/java/io/github/louistsaitszho/lineage/activities/MainActivity.kt
@@ -3,12 +3,12 @@ package io.github.louistsaitszho.lineage.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 import io.github.louistsaitszho.lineage.ItemListUnit
 import io.github.louistsaitszho.lineage.R
 import io.github.louistsaitszho.lineage.RecyclerViewAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,14 +22,8 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
+    //Layout option prone to change
     private var layoutOption: String? = null
-
-    //RecyclerView
-    private var recyclerView: RecyclerView? = null
-
-    //RecyclerView.Adapter
-    private var adapter: RecyclerView.Adapter<*>? = null
 
     //List holding the dummy information
     private var listItems: MutableList<ItemListUnit>? = null
@@ -75,6 +69,7 @@ class MainActivity : AppCompatActivity() {
         //Default layoutOption
         layoutOption = NO_THUMBNAIL
 
+
         //Method that Instantiates and runs the adapter
         runAdapter()
     }
@@ -83,32 +78,30 @@ class MainActivity : AppCompatActivity() {
     fun runAdapter(){
 
         //POOR IMPLEMENTATION DUE TO MY POOR KOTLIN SKILLS
-        var someSome: ItemListUnit = ItemListUnit("January","http://flaglane.com/download/german-flag/german-flag-large.png","This is the URL 2")
-        var someSome1: ItemListUnit = ItemListUnit("February ","http://flaglane.com/download/german-flag/german-flag-large.png","This is the URL 2")
-        var someSome2: ItemListUnit = ItemListUnit("March","https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Flag_of_Hong_Kong.svg/2000px-Flag_of_Hong_Kong.svg.png","This is the URL 2")
-        var someSome3: ItemListUnit = ItemListUnit("May","http://www.freepngimg.com/download/india/4-2-india-flag-png-hd.png","This is the URL 2")
-        var someSome4: ItemListUnit = ItemListUnit("June","https://upload.wikimedia.org/wikipedia/commons/1/17/Flag_of_Mexico.png","This is the URL 2")
-        var someSome5: ItemListUnit = ItemListUnit("July","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
-        var someSome6: ItemListUnit = ItemListUnit("August","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
-        var someSome7: ItemListUnit = ItemListUnit("September","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
-        var someSome8: ItemListUnit = ItemListUnit("October","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
-        var someSome9: ItemListUnit = ItemListUnit("Noviembre","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
-        var someSome10: ItemListUnit = ItemListUnit("Diciembre","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
-        var someSome11: ItemListUnit = ItemListUnit("THIS IS A VERY LONG STRING TO CHECK HOW LONG TEXT BEHAVES IN THIS SITUATION. IT'S ALSO FRIDAY TODAY, RAINY DAY","http://flaglane.com/download/german-flag/german-flag-large.png","This is the URL 2")
+        //Also, just a temporal implementation.
+        val someSome = ItemListUnit("January","http://flaglane.com/download/german-flag/german-flag-large.png","This is the URL 2")
+        val someSome1 = ItemListUnit("February ","http://flaglane.com/download/german-flag/german-flag-large.png","This is the URL 2")
+        val someSome2 = ItemListUnit("March","https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Flag_of_Hong_Kong.svg/2000px-Flag_of_Hong_Kong.svg.png","This is the URL 2")
+        val someSome3 = ItemListUnit("May","http://www.freepngimg.com/download/india/4-2-india-flag-png-hd.png","This is the URL 2")
+        val someSome4 = ItemListUnit("June","https://upload.wikimedia.org/wikipedia/commons/1/17/Flag_of_Mexico.png","This is the URL 2")
+        val someSome5 = ItemListUnit("July","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
+        val someSome6 = ItemListUnit("August","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
+        val someSome7 = ItemListUnit("September","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
+        val someSome8 = ItemListUnit("October","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
+        val someSome9 = ItemListUnit("Noviembre","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
+        val someSome10 = ItemListUnit("Diciembre","https://drive.google.com/open?id=0BzUGskuag7oNZHR2TFNsbnFxaU0","This is the URL 2")
+        val someSome11 = ItemListUnit("THIS IS A VERY LONG STRING TO CHECK HOW LONG TEXT BEHAVES IN THIS SITUATION. IT'S ALSO FRIDAY TODAY, RAINY DAY","http://flaglane.com/download/german-flag/german-flag-large.png","This is the URL 2")
 
         //List that will hold the dummy information
         listItems = mutableListOf(someSome, someSome1,someSome2,someSome3,someSome4,someSome5,someSome6, someSome7,someSome8,someSome9,someSome10,someSome11)
 
 
-        //Instantiating and setting recycler view attributes
-        recyclerView = findViewById(R.id.recycler_view)
-        (recyclerView)!!.setHasFixedSize(true)
-        (recyclerView)!!.setLayoutManager(LinearLayoutManager(this))
 
-        //Instantiating the Adapter with the List, The Context, and the Layout Size Setting
-        adapter = RecyclerViewAdapter(listItems, this, layoutOption)
+        //Instantiating the adapter with the magic of Kotlin:
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.setHasFixedSize(true)
+        //Instantiating the Adapter with the List, The Context, and the Layout Size Setting & Setting the adapter to Recycler View
+        recycler_view.adapter = RecyclerViewAdapter(listItems, this, layoutOption)
 
-        //Setting the adapter to the Recycler View
-        (recyclerView)!!.setAdapter(adapter)
     }
 }
