f2f9d347d812c70ec0aeac3cd57b7cdd53e6e1cf
Louis <louis993546@gmail.com>
2017-12-09 18:14:04 +0100

    add ci badge

--- app/src/main/java/io/github/louistsaitszho/lineage/services/VideosUpdateService.kt
+++ app/src/main/java/io/github/louistsaitszho/lineage/services/VideosUpdateService.kt
@@ -1,11 +1,25 @@
 package io.github.louistsaitszho.lineage.services
 
 import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.support.annotation.RequiresApi
+import android.support.v4.app.NotificationCompat
 import android.support.v4.content.ContextCompat
 import com.firebase.jobdispatcher.JobParameters
 import com.firebase.jobdispatcher.JobService
+import io.github.louistsaitszho.lineage.R
 import io.github.louistsaitszho.lineage.model.*
+
 
 /**
  * Check if there is any video that needs to be download
@@ -13,6 +27,7 @@ import io.github.louistsaitszho.lineage.model.*
  */
 class VideosUpdateService : JobService() {
     private val apiRequestSet = mutableListOf<Cancelable?>()
+    val channelId = "testing"
 
     override fun onStartJob(job: JobParameters): Boolean {
         //1. ask locally for module id(s) that needs update
@@ -36,10 +51,40 @@ class VideosUpdateService : JobService() {
                                                 if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
                                                     videoDownloader.downloadVideoNow(this@VideosUpdateService)
                                                 } else {
-                                                    //todo ask for permission, and after that resume download/schedule another video check
+                                                    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+                                                    //intent: open the settings page of the app
+                                                    val intent = Intent()
+                                                    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                                                    val uri = Uri.fromParts("package", packageName, null)
+                                                    intent.data = uri
+                                                    val pendingIntent = PendingIntent.getActivity(this@VideosUpdateService, 123, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+                                                    val builder = NotificationCompat.Builder(this@VideosUpdateService)
+                                                            .setSmallIcon(R.drawable.ic_app_logo)
+                                                            .setContentTitle("title")
+                                                            .setContentText("text")
+                                                            .setShowWhen(true)
+                                                            .setAutoCancel(true)
+                                                            .setContentIntent(pendingIntent)
+
+                                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                                        builder.setColor(ContextCompat.getColor(this@VideosUpdateService, R.color.primaryColor))
+                                                                .setCategory(Notification.CATEGORY_ERROR)
+                                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                                            val channel: NotificationChannel
+                                                            channel = generateNotificationChannel()
+                                                            notificationManager.createNotificationChannel(channel)
+                                                            builder.setChannelId(channelId)
+                                                        }
+                                                    }
+
+                                                    notificationManager.notify(1234, builder.build())
+                                                    jobFinished(job, false)
                                                 }
                                             }
                                         }
+                                        jobFinished(job, false)
                                     }
 
                                     override fun onFailure(error: Throwable) {
@@ -47,6 +92,7 @@ class VideosUpdateService : JobService() {
                                         //1) if sth about access token, show notification to ask user to sign in again
                                         //2) if just 500, retry later + log to somewhere
                                         //3) other failure case?
+                                        jobFinished(job, true)
                                     }
 
                                 })
@@ -57,7 +103,6 @@ class VideosUpdateService : JobService() {
                         }
                         //no need to call jobfinished here, the callbacks above will handle it
                     }
-                    //todo scroll through each module, check if they needs download
                 }
             }
 
@@ -68,6 +113,20 @@ class VideosUpdateService : JobService() {
         })
         apiRequestSet.add(cancelable)
         return true //so that all the callback above still works
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun generateNotificationChannel(): NotificationChannel {
+        val channelName = "Auto download video"
+        val channelDescription =  "blah blah blah"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(channelId, channelName, importance)
+        channel.description = channelDescription
+        channel.enableLights(true)
+        channel.lightColor = Color.WHITE
+        channel.setShowBadge(true)
+        channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+        return channel
     }
 
     override fun onStopJob(job: JobParameters?): Boolean {
